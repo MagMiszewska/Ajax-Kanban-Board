b58341b83b04cbe0a0d60daac4af356d7f5b96ad
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-05-18 10:29:02 +0200

    Fix scripts

--- js/Column.js
+++ js/Column.js
@@ -1,8 +1,8 @@
-function Column(name) {
+function Column(id, name) {
 	var self = this;
-	
-	this.id = randomString();
-	this.name = name;
+
+	this.id = id;
+	this.name = name || 'No name given';
 	this.element = createColumn();
 
 	function createColumn() {
@@ -11,31 +11,53 @@ function Column(name) {
 		var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
 		var columnCardList = $('<ul class="card-list"></ul>');
 		var columnDelete = $('<button class="btn-delete">x</button>');
-		var columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
-		
+		var columnAddCard = $('<button class="column-add-card">Add a card</button>');
+
 		// PODPINANIE ODPOWIEDNICH ZDARZEŃ POD WĘZŁY
-		columnDelete.click(function() {
+		columnDelete.click(function () {
 			self.deleteColumn();
 		});
-		
-		columnAddCard.click(function(event) {
+
+		//modyfikowany
+		columnAddCard.click(function (event) {
+			var cardName = prompt("Enter the name of the card");
 			event.preventDefault();
-			self.createCard(new Card(prompt("Wpisz nazwę karty")));
+			$.ajax({
+				url: baseUrl + '/card',
+				method: 'POST',
+				data: {
+					name: cardName,
+					bootcamp_kanban_column_id: self.id
+				},
+				success: function (response) {
+					var card = new Card(response.id, cardName);
+					self.createCard(card);
+				}
+			});
 		});
-			
-			// KONSTRUOWANIE ELEMENTU KOLUMNY
+
+		// KONSTRUOWANIE ELEMENTU KOLUMNY
 		column.append(columnTitle)
 			.append(columnDelete)
 			.append(columnAddCard)
 			.append(columnCardList);
-			return column;
-		}
+		return column;
 	}
+}
+
+//modyfikowany:
 Column.prototype = {
-	createCard: function(card) {
-	  this.element.children('ul').append(card.element);
+	createCard: function (card) {
+		this.element.children('ul').append(card.element);
 	},
-	deleteColumn: function() {
-	  this.element.remove();
+	deleteColumn: function () {
+		var self = this;
+		$.ajax({
+			url: baseUrl + '/column/' + self.id,
+			method: 'DELETE',
+			success: function (response) {
+				self.element.remove();
+			}
+		});
 	}
 };
